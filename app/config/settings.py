4b0a6840db750057c31b143f8895f8335bec5de9
Shahid Ali <mshahid@constellationhb.com>
2026-07-05 11:15:31 +0500

    Sprint 1 Day 2 Add application foundation and configuration module

---
+++ app/config/settings.py
@@ -0,0 +1,7 @@
+"""
+Application Settings
+"""
+
+APP_NAME = "Enterprise AI Toolkit"
+
+APP_VERSION = "0.1.0"
---
+++ app/main.py
@@ -0,0 +1,19 @@
+"""
+Enterprise AI Toolkit
+
+Author : Muhammad Ali
+Version : 0.1.0
+"""
+
+def main():
+
+    print("=" * 60)
+    print(" Enterprise AI Toolkit")
+    print(" Version : 0.1.0")
+    print("=" * 60)
+
+    print("Application Started Successfully")
+
+
+if __name__ == "__main__":
+    main()
